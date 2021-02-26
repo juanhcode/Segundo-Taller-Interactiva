8f667ebd84683b1ada907e1e85cbdbf805672f17
Juan Hoyos <64670953+juanhcode@users.noreply.github.com>
2021-02-26 18:45:36 -0500

    se agrego la interfaz , integrantes y archivo

---
+++ PrimerPuntoTallerInteractiva/src/BlackJack/JuegoVistaController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BlackJack;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javax.swing.JOptionPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Usuario
+ */
+public class JuegoVistaController implements Initializable {
+
+    @FXML
+    private MenuBar menuBar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void salirDelJuego(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    private void mostrarIntegrantes(ActionEvent event) {
+        JOptionPane ventana = new JOptionPane();
+        ventana.showMessageDialog(null,"Juan Manuel Hoyos Contreras" + "\n"
+                + "Sebastian Cifuentes Florez" + "\n"
+                + "Emanuel Benjumea Bejarano", "Quienes somos", JOptionPane.INFORMATION_MESSAGE);
+
+    }
+
+}
