915bb5c0aba1ef61e1e8efe7921fcb2891c9a04e
Juan Hoyos <64670953+juanhcode@users.noreply.github.com>
2021-02-27 18:49:44 -0500

    clase Paquete

---
+++ PrimerPuntoTallerInteractiva/src/BlackJack/PaqueteDeCartas.java
@@ -0,0 +1,17 @@
+package BlackJack;
+
+import java.util.Random;
+
+public class PaqueteDeCartas {
+    
+    private Carta paquete[];
+    private int cartaActual;
+    private final int NUMERO_CARTAS = 52;
+    private Random numerosAleatorios;
+
+    public PaqueteDeCartas(){
+        String caras[] = {"As","Dos","Tres","Cuatro","Cinco","Seis","Siete", "Ocho", "Nueve", "Diez", "Joto", "Quina", "Rey"};
+    }
+    
+    
+}
