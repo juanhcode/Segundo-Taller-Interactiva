df98a1dee71e1989cf19b1592715f06083d3e46c
Juan Hoyos <64670953+juanhcode@users.noreply.github.com>
2021-02-27 01:39:43 -0500

    clase Ficha

---
+++ PrimerPuntoTallerInteractiva/src/BlackJack/Ficha.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BlackJack;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ficha {
+    private int ficha1 = 1;
+    private int ficha5 = 5;
+    private int ficha10 = 10;
+    private int ficha25 = 25;
+    private int ficha50 = 50;
+    private int ficha100 = 100;
+    private int efectivo  = 1500;
+    
+    public Ficha(){
+        
+    }
+
+    public int getFicha1() {
+        return ficha1;
+    }
+
+    public int getFicha5() {
+        return ficha5;
+    }
+
+    public int getFicha10() {
+        return ficha10;
+    }
+
+    public int getFicha25() {
+        return ficha25;
+    }
+
+    public int getFicha50() {
+        return ficha50;
+    }
+
+    public int getFicha100() {
+        return ficha100;
+    }
+
+    public int getEfectivo() {
+        return efectivo;
+    }
+    
+    
+    
+    
+    
+}
